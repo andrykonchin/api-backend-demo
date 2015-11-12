651040f4e87d5e340f6321867ac38dd30dc15589
Andry Konchin <andry.konchin@gmail.com>
2015-11-12 15:12:35 +0200

    Add missed validation to Post model.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   has_attached_file :video
 
+  validates :title, :body, presence: true
   validates_attachment :video, content_type: { content_type: 'video/quicktime' }
 end
